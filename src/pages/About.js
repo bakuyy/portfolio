2719634d2997ce5be5ebb3ec08fc5463dbe64eb3
Sophie Yang <sophieyang12345@gmail.com>
2022-07-29 14:11:26 -0400

    minor changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,9 @@ const About = () => {
   return (
     <div>
         <div> 
-            <h1>Hey, I'm Sophie</h1>
+            <h1>Hey! I'm Sophie.</h1>
+            <h2> I'm a high school student at John Fraser SS and and aspriing software engineer.</h2>
+            
         </div>
     </div>
   )
